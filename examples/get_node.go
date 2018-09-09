948c2c7694fbdd3206e09c2c2878389f05756290
Evan Surdam <esurdam@gmail.com>
2018-09-08 21:17:40 -0700

    Add nodes example

---
+++ examples/get_node.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/esurdam/go-sophos"
+	"github.com/esurdam/go-sophos/api/v1.3.0/nodes"
+)
+
+//noinspection ALL
+var (
+	endpoint, token string
+)
+
+func init() {
+	if len(os.Args) == 2 {
+		endpoint = os.Args[1]
+		token = os.Args[2]
+	}
+
+	if endpoint == "" {
+		endpoint = os.Getenv("ENDPOINT")
+	}
+	if token == "" {
+		token = os.Getenv("TOKEN")
+	}
+
+	if endpoint == "" || token == "" {
+		panic("need endpoint and token as args or from env ($ENDPOINT, $TOKEN)")
+	}
+}
+
+func main() {
+	client, err := sophos.New(endpoint, sophos.WithAPIToken(token))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// using package func
+	port, err := nodes.GetWebadminPort(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(port)
+
+	// using node type
+	var wp nodes.WebadminPort
+	err = wp.Get(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(wp.Value)
+}
